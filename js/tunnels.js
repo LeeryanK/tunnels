619672abbdf86565faa59788e0c930045c686fa8
Kyle Lin <kylejlin.us@gmail.com>
2016-07-18 22:37:59 -0700

    Created TunnelsGame.prototype.resizeCanvas().

--- js/tunnels.js
+++ js/tunnels.js
@@ -12,20 +12,48 @@
     this.levelPlans = levelPlans;
     this.config = opt_config || this.config;
 
-    this.resize();
+    this.resizeCanvas();
+    window.addEventListener('resize', this.resizeCanvas.bind(this));
     this.init();
   }
 
-  TunnelsGame.prototype = {
-    canvasWidth: 800,
-    canvasHeight: 500,
+  TunnelsGame.prototype.canvasWidth = 800;
+  TunnelsGame.prototype.canvasHeight = 500;
+  TunnelsGame.prototype.canvasWidthToHeightRatio = 8 / 5;
 
-    config: {
+  TunnelsGame.prototype.config = {
 
-    },
+  };
 
-    resize: function() {
+  TunnelsGame.prototype.resizeCanvas = function() {
+    var windowWidthToHeightRatio = window.innerWidth / window.innerHeight;
 
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+
+    this.canvas.style.padding = 0;
+    this.canvas.style.border = 0;
+    this.canvas.style.margin = 0;
+    this.canvas.style.position = 'absolute';
+
+    if (windowWidthToHeightRatio > this.canvasWidthToHeightRatio) {
+      var scaledWidth = (window.innerWidth *
+        this.canvasWidthToHeightRatio) + 'px';
+      var remainingWidth = window.innerWidth - scaledWidth;
+
+      this.canvas.style.height = window.innerHeight + 'px';
+      this.canvas.style.width = scaledWidth;
+      this.canvas.style.top = 0;
+      this.canvas.style.left = ((remainingWidth / 2)|0) + 'px';
+    } else {
+      var scaledHeight = (window.innerHeight /
+        this.canvasWidthToHeightRatio) + 'px';
+      var remainingHeight = window.innerHeight - scaledHeight;
+
+      this.canvas.style.width = window.innerWidth + 'px';
+      this.canvas.style.height = scaledHeight;
+      this.canvas.style.left = 0;
+      this.canvas.style.top = ((remainingHeight / 2)|0) + 'px';
     }
   };
 })();
